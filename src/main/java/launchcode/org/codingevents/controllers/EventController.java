a8847fb789c59559aef4865a4a334c02b34492bb
Spillz <billygcasey@hotmail.com>
2020-02-20 13:12:53 -0600

    Initial commit https://www.youtube.com/watch?v=hmgxMOf51JU

---
+++ src/main/java/launchcode/org/codingevents/controllers/EventController.java
@@ -0,0 +1,37 @@
+package launchcode.org.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+    private static List<String> events = new ArrayList<>();
+
+    @GetMapping
+    public String displayAllEvents(Model model){
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+    //lives at /events/create https://www.youtube.com/watch?v=lgT962si4eQ
+    @GetMapping("create")
+    public String renderCreatEventForm(){
+        return "events/create";
+    }
+
+    //lives at /events/create https://www.youtube.com/watch?v=LnpJcq33uoM
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";  //redirects to displayAllEvents controller
+    }
+
+}
